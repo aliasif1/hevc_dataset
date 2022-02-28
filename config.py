0bfbd887178cfb0dcc07a156ade1c12f3236c450
Asif Ali Mehmuda <asif.mehmuda9@gmail.com>
2022-02-27 22:28:14 -0700

    config.py cleaned

--- config.py
+++ config.py
@@ -1,14 +1,19 @@
 # dictionery of video name and its location 
 videos = [
     {
-        'name':'vid_name',
-        'path': 'vid_path',
+        'name':'yacht_ride',
+        'path': '/home/ubuntu/Documents/VideosDataset/YachtRide_3840x2160_120fps_420_8bit_YUV_Y4M/YachtRide_3840x2160.y4m',
+        'resolution': '3840x2160'
+    },
+    {
+        'name':'river_bank',
+        'path':'/home/ubuntu/Documents/VideosDataset/RiverBank_3840x2160_50fps_420_8bit_YUV_RAW/RiverBank_3840x2160_50fps_8bit.y4m',
         'resolution': '3840x2160'
     },
 ]
 
 # the segment length to split the video into
-segmentLenghts = [1,2]
+segmentLenghts = [1, 2]
 
 # the codecs to consider  
 codecs = ['hevc']
@@ -23,19 +28,20 @@ targetResolutions = [
 ]
 
 # crf values to consider 
-crfValues = [16,22,28]
+crfValues = [16,18,20,22,24,26,28]
+
 
 # the absolute path where the encoded videos should be stored
-encodedVideoDirectoryPath = 'path'
+encodedVideoDirectoryPath = '/home/ubuntu/Documents/VideosDataset/EncodedSegments3'
 
 # the absolute results path 
-resultsDirectoryPath = 'path'
+resultsDirectoryPath = '/home/ubuntu/Documents/VideosDataset/Results'
 
 # the results file name
-resultsFile = 'result.csv'
+resultsFile = 'Result.csv'
 
 # Raw Segments directory. Wheret the raw split segmnets will go to
-rawSegmentsDirectoryPath = 'path'
+rawSegmentsDirectoryPath = '/home/ubuntu/Documents/VideosDataset/RawSegments'
 
 # preset
 preset = 'ultrafast'
